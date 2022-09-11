13fb692046d6f1d2f2da8a2690e41512d62e4cdd
AlexanderKudr <trevan98@gmail.com>
2022-09-12 02:36:05 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { MovieCard } from "./components/MovieCard";
 import "./App.scss";
+import SearchIcon from "./assets/search.svg";
 
-// const Person = ({ name, lastName, age }) => {
-//   return (
-//     <>
-//       <h1>Name: {name}</h1>
-//       <h2>Last Name: {lastName}</h2>
-//       <h2>Age: {age}</h2>
-//     </>
-//   );
-// };
+const API_URL = "http://www.omdbapi.com?apikey=7ddbb20";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+export const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    alert("You changed the counter to " + count)
-  }, [])
-  const increment = () => {
-    setCount(prevCount => prevCount + 1);
-  }
-  const decrement = () => {
-    setCount(prevCount => prevCount - 1);
-  }
+  const searchMovies = async () => {
+    const response = await fetch(`${API_URL}&s=${search}`);
+    const { Search } = await response.json();
+    setMovies(Search);
+  };
+
+  const inputChangeHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <button onClick={increment}>+</button>
-      <h1>{count}</h1>
-      <button onClick={decrement}>-</button>
+    <div className="app">
+      <h1>MovieYay!</h1>
+      <div className="search">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault(); //method prevent page refresh
+            searchMovies(); //call function that you need
+          }}
+        >
+          <input
+            onChange={inputChangeHandler}
+            placeholder="Search for movies"
+          />
+          <img src={SearchIcon} alt="search" onClick={searchMovies} />
+        </form>
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, index) => (
+            <MovieCard key={index} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
-
-export default App;
